a45b6a8b779d82c6e41f662dd642b744da03d40a
youss95 <usekim7555@gmail.com>
2023-02-04 21:22:43 +0900

    commit

---
+++ batch-spring/src/test/java/com/ksy/batchspring/BatchSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.batchspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
